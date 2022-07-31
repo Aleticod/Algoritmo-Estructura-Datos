da2d5f32fc797cc6573d6d943d575730c9675603
Harvey Unsaac <193837@unsaac.edu.pe>
2022-07-31 05:48:35 +0200

    Se añadio Lista recursiva dentro de la libreria y su aplicacion

---
+++ AppListaRecursiva/AppListaRecursiva/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using EstructuraDatosLineales;
+
+namespace AppListaRecursiva
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CLista lista = new CLista();
+
+            lista.agregar(0);
+            lista.agregar(1);
+            lista.agregar(2);
+            lista.agregar(3);
+            lista.agregar(4);
+            lista.agregar(6);
+            //Console.WriteLine(lista.longitud);
+            //lista.insertar(5, 5);
+            //lista.mostrar();
+            //Console.WriteLine(lista.longitud);
+            //lista.eliminar();
+            //lista.mostrar();
+            //lista.eliminarIesimo(2);
+            //lista.mostrar();
+            //Console.WriteLine(lista.ubicacion(4));
+            lista.iesimo(3);
+
+        }
+        
+    }
+}
---
+++ EstructuraDatosLineales/CLista.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstructuraDatosLineales
+{
+    public class CLista
+    {
+        // Atributos
+        private Object aElemento;
+        private CLista aSublista;
+
+        // Metodo constructor
+        public CLista()
+        {
+            aElemento = null;
+            aSublista = null;
+        }
+
+        public CLista(Object pElemento, CLista pLista)
+        {
+            aElemento = pElemento;
+            aSublista = pLista;
+        }
+
+        
+
+        // Propiedades set y get
+        public Object Elemento
+        {
+            get { return aElemento; }
+            set { aElemento = value; }
+        }
+
+        public CLista Sublista
+        {
+            get { return aSublista; }
+            set { aSublista = value; }
+        }
+
+        // Propiedad longitud
+        public int longitud
+        {
+            get
+            {
+                if (aElemento == null)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return aSublista.longitud + 1;
+                }
+            }
+        }
+
+        // Metodos auxiliares (Comportamiento)
+
+        public bool esVacia()
+        {
+            return aElemento == null && aSublista == null;
+        }
+
+        public void agregar(Object pElemento)
+        {
+            if (esVacia())
+            {
+                Elemento = pElemento;
+                aSublista = new CLista();
+            }
+            else
+            {
+                aSublista.agregar(pElemento);
+            }
+        }
+
+        public void insertar(Object pElemento, int indice)
+        {
+            if (indice == 0)
+            {
+                Object aux = aElemento;
+                CLista auxLista = aSublista;
+                Elemento = pElemento;
+                Sublista = new CLista(aux, aSublista);
+            }
+            else
+            {
+                aSublista.insertar(pElemento, indice -  1);
+            }
+        }
+        
+        public void mostrar ()
+        {
+            if(!esVacia())
+            {
+                Console.WriteLine(Elemento.ToString());
+                aSublista.mostrar();
+            }
+        }
+
+        public void eliminar()
+        {
+            if (!esVacia())
+            {
+                if (aSublista.Elemento == null)
+                {
+                    aElemento = null;
+                    aSublista = null;
+                }
+                else
+                {
+                    aSublista.eliminar();
+                }
+            }
+        }
+
+        public void eliminarIesimo ( int indice)
+        {
+            if(indice == 0)
+            {
+                aElemento = aSublista.aElemento;
+                aSublista = aSublista.aSublista;
+            }
+
+            else
+            {
+                aSublista.eliminarIesimo(indice - 1);
+            }
+        }
+
+        public int ubicacion (Object pElemento)
+        {   
+            if (!esVacia())
+            {
+                if(pElemento.Equals(aElemento))
+                {
+                    return 0;
+                }
+                else
+                {
+                    return aSublista.ubicacion(pElemento) + 1;
+                }
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public void iesimo(int indice)
+        {
+            if(!esVacia())
+            {
+                if(indice == 0)
+                {
+                    Console.WriteLine(aElemento.ToString());
+                }
+                else
+                {
+                    aSublista.iesimo(indice - 1);
+                }
+            }
+        }
+
+    }
+}
